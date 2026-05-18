c600b5fc28c288bac4a943fce7949df183c07422
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-18 17:36:13 -0300

    feat: implementa o controller de produto

---
+++ src/main/java/br/com/arthyxx/controllers/ProdutoController.java
@@ -0,0 +1,56 @@
+package br.com.arthyxx.controllers;
+
+import br.com.arthyxx.dto.produto.CreateProdutoDTO;
+import br.com.arthyxx.dto.produto.PatchProdutoDTO;
+import br.com.arthyxx.dto.produto.ProdutoResponseDTO;
+import br.com.arthyxx.dto.produto.PutProdutoDTO;
+import br.com.arthyxx.services.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<ProdutoResponseDTO> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProdutoResponseDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProdutoResponseDTO create(@RequestBody @Valid CreateProdutoDTO dto){
+        return service.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ProdutoResponseDTO update(@PathVariable Long id, @RequestBody @Valid PutProdutoDTO dto){
+        return service.update(id, dto);
+    }
+
+    @PatchMapping("/{id}")
+    public ProdutoResponseDTO partialUpdate(@PathVariable Long id, @RequestBody @Valid PatchProdutoDTO dto){
+        return service.partialUpdate(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
