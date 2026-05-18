7febfb59ff59ae069f16cf7622ee0dfd4a81b134
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-18 15:42:55 -0300

    feat: implementa Service em Cliente

--- src/main/java/br/com/arthyxx/mapper/ClienteMapper.java
+++ src/main/java/br/com/arthyxx/mapper/ClienteMapper.java
@@ -2,12 +2,26 @@ package br.com.arthyxx.mapper;
 
 import br.com.arthyxx.dto.cliente.ClienteResponseDTO;
 import br.com.arthyxx.dto.cliente.CreateClienteDTO;
+import br.com.arthyxx.dto.cliente.PatchClienteDTO;
+import br.com.arthyxx.dto.cliente.PutClienteDTO;
 import br.com.arthyxx.models.Cliente;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
     Cliente toEntity(CreateClienteDTO dto);
 
     ClienteResponseDTO toResponseDTO(Cliente cliente);
+
+    List<ClienteResponseDTO> toResponseDTOList(List<Cliente> clientes);
+
+    void updateFromPutDTO(PutClienteDTO dto, @MappingTarget Cliente cliente);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromPatchDTO(PatchClienteDTO dto, @MappingTarget Cliente cliente);
 }
---
+++ src/main/java/br/com/arthyxx/services/ClienteService.java
@@ -0,0 +1,75 @@
+package br.com.arthyxx.services;
+
+import br.com.arthyxx.dto.cliente.ClienteResponseDTO;
+import br.com.arthyxx.dto.cliente.CreateClienteDTO;
+import br.com.arthyxx.dto.cliente.PatchClienteDTO;
+import br.com.arthyxx.dto.cliente.PutClienteDTO;
+import br.com.arthyxx.mapper.ClienteMapper;
+import br.com.arthyxx.models.Cliente;
+import br.com.arthyxx.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    private final ClienteRepository repository;
+    private final ClienteMapper mapper;
+
+    public ClienteService(ClienteRepository repository, ClienteMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<ClienteResponseDTO> findAll(){
+        List<Cliente> entities = repository.findAll();
+
+        return mapper.toResponseDTOList(entities);
+    }
+
+    public ClienteResponseDTO findById(Long id){
+        Cliente entity = repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Cliente não encontrado!")
+        );
+
+        return mapper.toResponseDTO(entity);
+    }
+
+    public ClienteResponseDTO create(CreateClienteDTO dto){
+        Cliente entity = mapper.toEntity(dto);
+
+        return mapper.toResponseDTO(repository.save(entity));
+    }
+
+    public ClienteResponseDTO update(Long id, PutClienteDTO dto){
+        Cliente entity = repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Cliente não encontrado!")
+        );
+
+        mapper.updateFromPutDTO(dto, entity);
+
+        Cliente updatedEntity = repository.save(entity);
+
+        return mapper.toResponseDTO(updatedEntity);
+    }
+
+    public ClienteResponseDTO partialUpdate(Long id, PatchClienteDTO dto){
+        Cliente entity = repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Cliente não encontrado!")
+        );
+
+        mapper.updateFromPatchDTO(dto, entity);
+
+        Cliente updatedEntity = repository.save(entity);
+
+        return mapper.toResponseDTO(updatedEntity);
+    }
+
+    public void delete(Long id){
+        Cliente entity = repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Cliente não encontrado!")
+        );
+
+        repository.delete(entity);
+    }
+}
