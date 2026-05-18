ebe506584c6163195b70a302676da65a1cff3363
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-18 16:41:06 -0300

    feat: implementa Produto e Categoria repository

---
+++ src/main/java/br/com/arthyxx/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.arthyxx.repository;
+
+import br.com.arthyxx.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
---
+++ src/main/java/br/com/arthyxx/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.arthyxx.repository;
+
+import br.com.arthyxx.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
