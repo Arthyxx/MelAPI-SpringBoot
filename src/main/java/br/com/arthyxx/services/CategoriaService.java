f1c42184104ae011863a38d9236754a921ebb503
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-19 14:38:38 -0300

    feat: implementa BusinessException para categoria duplicada

--- src/main/java/br/com/arthyxx/services/CategoriaService.java
+++ src/main/java/br/com/arthyxx/services/CategoriaService.java
@@ -4,6 +4,7 @@ import br.com.arthyxx.dto.categoria.CategoriaResponseDTO;
 import br.com.arthyxx.dto.categoria.CreateCategoriaDTO;
 import br.com.arthyxx.dto.categoria.PatchCategoriaDTO;
 import br.com.arthyxx.dto.categoria.PutCategoriaDTO;
+import br.com.arthyxx.exceptions.BusinessException;
 import br.com.arthyxx.exceptions.ResourceNotFoundException;
 import br.com.arthyxx.mapper.CategoriaMapper;
 import br.com.arthyxx.models.Categoria;
@@ -37,6 +38,8 @@ public class CategoriaService {
     }
 
     public CategoriaResponseDTO create(CreateCategoriaDTO dto){
+        if (repository.existsByName(dto.name())) throw new BusinessException("Já existe uma categoria com esse nome.");
+
         Categoria entity = mapper.toEntity(dto);
 
         return mapper.toResponseDTO(repository.save(entity));
@@ -47,6 +50,8 @@ public class CategoriaService {
                 () -> new ResourceNotFoundException("Categoria não encontrado!")
         );
 
+        if (repository.existsByNameAndIdNot(dto.name(), id)) throw new BusinessException("Já existe outra categoria com esse nome.");
+
         mapper.updateFromPutDTO(dto, entity);
 
         return mapper.toResponseDTO(repository.save(entity));
@@ -56,6 +61,10 @@ public class CategoriaService {
         Categoria entity = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Categoria não encontrado!")
         );
+
+        if (dto.name() != null && repository.existsByNameAndIdNot(dto.name(), id)){
+            throw new BusinessException("Já existe outra categoria com esse nome.");
+        }
 
         mapper.updateFromPatchDTO(dto, entity);
 
