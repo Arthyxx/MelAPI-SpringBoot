604b4d57e0bffff14b09884140b48421eed45a9a
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-19 15:22:35 -0300

    feat: implementa PedidoRepository

---
+++ src/main/java/br/com/arthyxx/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.arthyxx.repository;
+
+import br.com.arthyxx.models.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
