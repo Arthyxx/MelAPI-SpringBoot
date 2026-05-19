b35c3865cba94df95104000567d708c07a8d4d12
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-05-19 14:53:13 -0300

    feat: ajeitando o target e adicionando métodos

--- src/main/java/br/com/arthyxx/mapper/ProdutoMapper.java
+++ src/main/java/br/com/arthyxx/mapper/ProdutoMapper.java
@@ -1,9 +1,11 @@
 package br.com.arthyxx.mapper;
 
+import br.com.arthyxx.dto.categoria.CategoriaResumoResponseDTO;
 import br.com.arthyxx.dto.produto.CreateProdutoDTO;
 import br.com.arthyxx.dto.produto.PatchProdutoDTO;
 import br.com.arthyxx.dto.produto.ProdutoResponseDTO;
 import br.com.arthyxx.dto.produto.PutProdutoDTO;
+import br.com.arthyxx.models.Categoria;
 import br.com.arthyxx.models.Produto;
 import org.mapstruct.*;
 
@@ -11,11 +13,14 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProdutoMapper {
+    @Mapping(target = "category", ignore = true)
     Produto toEntity(CreateProdutoDTO dto);
 
     ProdutoResponseDTO toResponseDTO(Produto produto);
 
     List<ProdutoResponseDTO> toResponseDTOList(List<Produto> produtos);
+
+    CategoriaResumoResponseDTO toCategoriaResumoResponseDTO(Categoria categoria);
 
     @Mapping(target = "category", ignore = true)
     void updateFromPutDTO(PutProdutoDTO dto, @MappingTarget Produto produto);
