f1c7d5c6904d41ad850a7e78833a9d1d9a524186
Arthyxx <arthur.limag.alu.lmb@gmail.com>
2026-05-20 09:57:57 -0300

    feat: implementa SecurityFilter

---
+++ src/main/java/br/com/arthyxx/config/SecurityFilter.java
@@ -0,0 +1,59 @@
+package br.com.arthyxx.config;
+
+import br.com.arthyxx.models.Cliente;
+import br.com.arthyxx.repository.ClienteRepository;
+import br.com.arthyxx.services.auth.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final ClienteRepository clienteRepository;
+
+    public SecurityFilter(TokenService tokenService, ClienteRepository clienteRepository) {
+        this.tokenService = tokenService;
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if (token != null){
+            String email = tokenService.validateToken(token);
+
+            Cliente cliente = clienteRepository.findByEmail(email).orElse(null);
+
+            if (cliente != null){
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        cliente,
+                        null,
+                        cliente.getAuthorities()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null){
+            return null;
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
