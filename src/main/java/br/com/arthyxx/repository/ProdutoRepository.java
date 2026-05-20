52dc321526516e33a3725054e7b1930a485bf086
Arthyxx <arthur.limag.alu.lmb@gmail.com>
2026-05-20 08:13:02 -0300

    feat: implementa Querys com filtros para Produto

--- src/main/java/br/com/arthyxx/repository/ProdutoRepository.java
+++ src/main/java/br/com/arthyxx/repository/ProdutoRepository.java
@@ -1,7 +1,11 @@
 package br.com.arthyxx.repository;
 
 import br.com.arthyxx.models.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +13,17 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     boolean existsByName(String name);
 
     boolean existsByNameAndIdNot(String name, Long id);
+
+    @Query("""
+            SELECT p FROM Produto p
+            WHERE (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
+            AND (:categoryId IS NULL OR p.category.id = :categoryId)
+            AND (:active IS NULL OR p.active = :active)
+            """)
+    Page<Produto> findAllWithFilter(
+            @Param("name") String name,
+            @Param("categoryId") Long categoryId,
+            @Param("active") Boolean active,
+            Pageable pageable
+    );
 }
